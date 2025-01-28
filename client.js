9c2ee2f2f7826232b3537f338bddc1aadf3f3b5f
LYoooJ <limyoojin@postech.ac.kr>
2025-01-28 14:36:01 +0100

    add profile view

--- client.js
+++ client.js
@@ -5,5 +5,8 @@ displayView = function(){
     //code that is executed as the page is loaded.
     //You shall put your own custom code here.
     //window.alert() is not allowed to be used in your implementation.
-    window.alert("Hello TDDD97!");
+
+    let welcomeView = document.getElementById("welcomeview");
+    document.getElementById("view").innerHTML += welcomeView.innerHTML;
+    // window.alert("Hello TDDD97!");
     };
