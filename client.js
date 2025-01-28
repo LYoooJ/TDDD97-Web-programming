fb2c68b6017d48867aa28b0d07c7683e2b4207f8
LYoooJ <limyoojin@postech.ac.kr>
2025-01-28 15:48:17 +0100

    add validation

--- client.js
+++ client.js
@@ -1,12 +1,48 @@
 displayView = function(){
     // the code required to display a view
-    };
-    window.onload = function(){
-    //code that is executed as the page is loaded.
-    //You shall put your own custom code here.
-    //window.alert() is not allowed to be used in your implementation.
+};
 
-    let welcomeView = document.getElementById("welcomeview");
-    document.getElementById("view").innerHTML += welcomeView.innerHTML;
-    // window.alert("Hello TDDD97!");
-    };
+window.onload = function(){
+//code that is executed as the page is loaded.
+//You shall put your own custom code here.
+//window.alert() is not allowed to be used in your implementation.
+
+let welcomeView = document.getElementById("welcomeview");
+document.getElementById("view").innerHTML += welcomeView.innerHTML;
+// window.alert("Hello TDDD97!");
+};
+
+function matchPassword() {
+    let psw = document.getElementById("password_signup").value;
+    let rePsw = document.getElementById("repeatPSW").value;
+    if (psw != rePsw) {
+        return false;
+    }
+    return true;
+};
+
+function minLengthPassword() {
+    let psw = document.getElementById("password_signup").value;
+    let rePsw = document.getElementById("repeatPSW").value;    
+
+    if (psw.length < 8 || rePsw.length < 8) {
+        return false;
+    }
+    return true;
+}
+
+function validate() {
+    let errorMsg = document.getElementById("error_message");
+    
+    if (!matchPassword()) {
+        errorMsg.textContent = "Your passwords must be the same.";
+        return false;
+    }
+    if (!minLengthPassword()) {
+        errorMsg.textContent = "Password should be at least 8 characters.";
+        return false;
+    }
+    errorMsg.textContent = "";
+    return true;
+    
+};
